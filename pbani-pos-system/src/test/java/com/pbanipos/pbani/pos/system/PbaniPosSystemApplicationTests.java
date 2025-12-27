6fbb24afc14bd2f2709c838c1d980c2517fabb6b
palbani <albani.paul1@gmail.com>
2025-12-27 13:50:58 -0500

    initial commit for pos system

---
+++ pbani-pos-system/src/test/java/com/pbanipos/pbani/pos/system/PbaniPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbanipos.pbani.pos.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbaniPosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
